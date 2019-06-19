b9c301109331744a7e67d481e9e1725163adf9e8
YuLun Shih <shih@yulun.me>
2019-06-19 01:09:45 -0700

    Add exodus bundle

--- build.sh
+++ build.sh
@@ -6,7 +6,21 @@ env
 # Install Deps
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get -o Dpkg::Options::="--force-confold"  install -y curl build-essential pkg-config libcurl4-openssl-dev git cmake libssl-dev uuid-dev valgrind
+apt-get -o Dpkg::Options::="--force-confold" install -y \
+  python \
+  python-pip
+  curl \
+  build-essential \
+  pkg-config \
+  libcurl4-openssl-dev \
+  git \
+  cmake \
+  libssl-dev \
+  uuid-dev \
+  valgrind
+
+pip install -U pip
+pip install exodus-bundler
 
 # Build
 git clone --recursive --depth 1 --branch "$SDK_TAG" https://github.com/Azure/azure-iot-sdk-c.git
@@ -15,4 +29,6 @@ mkdir cmake
 cd cmake
 cmake -Duse_prov_client:BOOL=ON -Duse_tpm_simulator:BOOL="$TPM_SIMULATOR" ..
 make
-mv provisioning_client/tools/tpm_device_provision/tpm_device_provision "/output/tpm_device_provision-simulator_$TPM_SIMULATOR-$ARCH"
+
+# Create exodus bundle
+exodus provisioning_client/tools/tpm_device_provision/tpm_device_provision --output "/output/tpm_device_provision-simulator_$TPM_SIMULATOR-$ARCH"
