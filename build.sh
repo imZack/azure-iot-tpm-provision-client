b323e16abfe06d9fe21f4d8b695bda52d5a99b1c
YuLun Shih <shih@yulun.me>
2019-06-18 22:25:29 -0700

    Init

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+env
+
+# Install Deps
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get -o Dpkg::Options::="--force-confold"  install -y curl build-essential pkg-config libcurl4-openssl-dev git cmake libssl-dev uuid-dev valgrind
+
+# Build
+git clone --recursive --depth 1 --branch "$SDK_TAG" https://github.com/Azure/azure-iot-sdk-c.git
+cd azure-iot-sdk-c
+mkdir cmake
+cd cmake
+cmake -Duse_prov_client:BOOL=ON -Duse_tpm_simulator:BOOL="$TPM_SIMULATOR" ..
+make
+mv provisioning_client/tools/tpm_device_provision/tpm_device_provision "/output/tpm_device_provision-simulator_$TPM_SIMULATOR-$ARCH"
